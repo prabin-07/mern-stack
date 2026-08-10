ece2fe61914e71bab2f191f31d22d2f36207e7f6
prabin-07 <prabinpradeep679@gmail.com>
2026-08-10 12:10:01 +0530

    array

---
+++ arrays/2darray.js
@@ -0,0 +1,12 @@
+//Array operations
+let arr = [10, 20, 30, 40, 50];
+arr.push(60);  //push
+console.log("Push:", arr);
+arr.pop();  //pop
+console.log("pop:", arr);
+arr.unshift(5);  //unshift
+console.log("unshift:", arr);
+arr.shift();  //shift
+console.log("Shift:", arr);
+arr.splice(1, 2, 99, 88);  //splice
+console.log("Splice:", arr);
